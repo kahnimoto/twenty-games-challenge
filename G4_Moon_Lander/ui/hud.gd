6502c9f2f618beb1b58185cb5fc6b0c65363da68
Kahn <255939722+kahnimoto@users.noreply.github.com>
2026-01-26 13:01:34 +0100

    showing velocity in hud

--- G4_Moon_Lander/ui/hud.gd
+++ G4_Moon_Lander/ui/hud.gd
@@ -3,6 +3,9 @@ extends CanvasLayer
 var lander: Lander
 
 @onready var fuel_gauge: TextureProgressBar = %FuelGauge
+@onready var velocity_x: ProgressBar = %VelocityX
+@onready var velocity_y: ProgressBar = %VelocityY
+@onready var velocity_length: ProgressBar = %VelocityLength
 
 
 
@@ -15,3 +18,18 @@ func _update_fuel_gauge(new_value: float) -> void:
 func _process(_delta: float) -> void:
 	if lander:
 		_update_fuel_gauge(lander.fuel)
+		#velocity_x.value = abs(lander.linear_velocity.x)
+		#velocity_y.value = abs(lander.linear_velocity.y)
+		var incoming_speed: float = lander.linear_velocity.abs().length()
+		var ratio: float = 0.0
+		if incoming_speed < 1.0:
+			ratio = remap(incoming_speed, 0.0, 1.0, 0.0, 10.0)
+		elif incoming_speed < 10.0:
+			ratio = remap(incoming_speed, 1.0, 10.0, 10.0, 20.0)
+		else:
+			ratio = remap(clampf(incoming_speed, 0.0, 100.0), 10.0, 100.0, 20.0, 100.0)
+		if incoming_speed > Lander.ACCEPTABLE_LANDING_SPEED:
+			velocity_length.modulate = Color.RED
+		else:
+			velocity_length.modulate = Color.GREEN
+		velocity_length.value = ratio

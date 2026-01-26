0f2b8bb0bb911989e089a033f007eca2a09a427e
Kahn <255939722+kahnimoto@users.noreply.github.com>
2026-01-26 13:00:52 +0100

    pushing character with force in all directions

--- G4_Moon_Lander/nodes/landing_pad.gd
+++ G4_Moon_Lander/nodes/landing_pad.gd
@@ -1,9 +1,13 @@
+class_name LandingPad
 extends StaticBody2D
+
+const TIME_DELAY_BEFORE_REWARD := 1.0
 
 @export var lander: Lander
 @export_enum("START", "FUEL", "GOAL") var type: String = "START"
 
 var lander_in_area := false
+var time_on_ground := 0.0
 
 @onready var area: Area2D = $Area2D
 
@@ -17,16 +21,23 @@ func _process(delta: float) -> void:
 	if not lander_in_area:
 		return
 	assert(lander is Lander)
-	#print(lander.velocity)
-	#if lander.velocity == Vector2(0.0, 50.0):
-		#match type:
-			#"START":
-				#pass
-			#"FUEL":
-				#lander.fuel += 20 * delta
-			#"GOAL":
-				#print("YOU WIN")
+	
+	#lander.angular_damp = 30.0
+	#lander.linear_damp = 30.0
+	if abs(lander.angular_velocity) < 0.05 and abs(lander.linear_velocity.x) < 0.05 and abs(lander.linear_velocity.y) < 0.05:
+		time_on_ground += delta
+	if lander.on_ground:
+		match type:
+			"START":
+				pass
+			"FUEL":
+				if time_on_ground > TIME_DELAY_BEFORE_REWARD:
+					lander.fuel += 20 * delta
+			"GOAL":
+				if time_on_ground > TIME_DELAY_BEFORE_REWARD:
+					print("YOU WIN")
 
 func _on_lander_on_pad(other: Area2D, entering: bool):
 	assert(other.is_in_group("lander_detector"))
 	lander_in_area = entering
+	time_on_ground = 0.0
--- G4_Moon_Lander/player/player.gd
+++ G4_Moon_Lander/player/player.gd
@@ -1,27 +1,35 @@
 class_name Lander
-extends CharacterBody2D
+extends RigidBody2D
 
 
-const TURN_SPEED = 1.4
-const THRUST = 7000.0
-const GRAVITY = 3000.0
+const TURN_SPEED = 150.0
+const UP_THRUST = 50.0
+const SIDE_THRUST = 30.0
+const GRAVITY = 20.0
 const ROTATION_FORGIVNESS_WHEN_LANDING := 0.7
 const FUEL_USED_PER_SECOND := 15.0
-const FUEL_GAINED_PER_SECOND := 3.0
+const FUEL_GAINED_PER_SECOND := 15.0 
+const ACCEPTABLE_LANDING_SPEED := 40
 
 var _land_gears_out := true
+var on_ground := false
 var dead := false
 var fuel := 100.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var land_gear_detector: Area2D = %LandGearDetector
-@onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D
+@onready var thrust_particles: GPUParticles2D = $ThrustParticles
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var left_thrust_particles: GPUParticles2D = $LeftThrustParticles
+@onready var right_thrust_particles: GPUParticles2D = $RightThrustParticles
+@onready var rotate_left_thrust_particles: GPUParticles2D = $RotateLeftThrustParticles
+@onready var rotate_right_thrust_particles: GPUParticles2D = $RotateRightThrustParticles
 
 
 func _ready() -> void:
 	land_gear_detector.body_entered.connect(_on_land_detected.bind(true))
 	land_gear_detector.body_exited.connect(_on_land_detected.bind(false))
+	body_entered.connect(_on_body_entered)
 	Hud.lander = self
 
 
@@ -31,6 +39,7 @@ func _on_land_detected(_body, landing: bool) -> void:
 			animated_sprite_2d.play_backwards("landgears")
 			_land_gears_out = true
 	else:
+		#on_ground = false
 		if _land_gears_out:
 			animated_sprite_2d.play("landgears")
 			_land_gears_out = false
@@ -48,35 +57,75 @@ func _toggle_landgears() -> void:
 			animated_sprite_2d.play("landgears")
 		_land_gears_out = not _land_gears_out
 
+var new_location := Vector2.ZERO
+var previous_location := Vector2.ZERO
+var actual_velocity := Vector2.ZERO
+var copy_linear_velocity := Vector2.ZERO
+var previous_linear_velocity := Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
+	previous_location = new_location
+	new_location = global_position
+	var actual_difference = new_location - previous_location
+	actual_velocity = actual_difference * 60
+	previous_linear_velocity = copy_linear_velocity
+	copy_linear_velocity = linear_velocity
+   
 	if dead:
 		return
 	if fuel <= 0.0:
 		explode()
-	velocity = Vector2.DOWN * GRAVITY * delta
+	if not on_ground:
+		apply_force(Vector2.DOWN * GRAVITY)
 
-	if Input.is_action_pressed("up"):
-		velocity += Vector2.UP.rotated(rotation) * THRUST * delta
-		gpu_particles_2d.emitting = true
+	if Input.is_action_pressed("thrust"):
+		apply_force(Vector2.UP.rotated(rotation) * UP_THRUST)
+		thrust_particles.emitting = true
 		fuel = clampf(fuel - (delta * FUEL_USED_PER_SECOND), 0.0, 100.0)
 	else:
-		gpu_particles_2d.emitting = false
-		#fuel = clampf(fuel + (delta * FUEL_GAINED_PER_SECOND), 0.0, 100.0)
+		thrust_particles.emitting = false
+		fuel = clampf(fuel + (delta * FUEL_GAINED_PER_SECOND), 0.0, 100.0)
 
-	var turn := Input.get_axis("left", "right")
-	if turn:
-		rotate(turn * delta * TURN_SPEED)
+	left_thrust_particles.emitting = false
+	right_thrust_particles.emitting = false
+	var sideways := Input.get_axis("move_left", "move_right")
+	if not on_ground and sideways:
+		left_thrust_particles.emitting = sideways < 0.
+		right_thrust_particles.emitting = sideways > 0.
+		var dir: Vector2 = Vector2.LEFT if sideways < 0 else Vector2.RIGHT
+		apply_force(dir.rotated(rotation) * SIDE_THRUST, Vector2(0., -2.))
+		fuel = clampf(fuel - (delta * FUEL_USED_PER_SECOND), 0.0, 100.0)
+
+	rotate_left_thrust_particles.emitting = false
+	rotate_right_thrust_particles.emitting = false
+	if on_ground:
+		if rotation < -0.05:
+			apply_torque(TURN_SPEED)
+		elif rotation > 0.05:
+			apply_torque(-TURN_SPEED)
+	else:
+		var turn := Input.get_axis("rotate_left", "rotate_right")
+		if turn:
+			rotate_left_thrust_particles.emitting = turn < 0.
+			rotate_right_thrust_particles.emitting = turn > 0.
+			apply_torque(turn * TURN_SPEED)
+			fuel = clampf(fuel - (delta * FUEL_USED_PER_SECOND), 0.0, 100.0)
 	
-	var c: KinematicCollision2D = move_and_collide(velocity * delta)
-	if c:
-		var collided_with: Node2D = c.get_collider() as Node2D
-		if collided_with.is_in_group("landingpad"):
-			if (abs(rotation) > ROTATION_FORGIVNESS_WHEN_LANDING):
-				explode()
-			else:
-				create_tween().tween_property(self, "rotation", 0.0, 0.15).set_ease(Tween.EASE_OUT)
-		else:
+	if get_contact_count() == 0:
+		on_ground = false
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is LandingPad:
+		var incoming_speed: float = previous_linear_velocity.abs().length()
+		if incoming_speed > 0.01:
+			print(incoming_speed)
+		on_ground = true
+		if incoming_speed > ACCEPTABLE_LANDING_SPEED:
+			explode()
+	else:
+		if not dead:
+			dead = true
 			explode()
 
 

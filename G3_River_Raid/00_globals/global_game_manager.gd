6f1f64828c635f5bbb6013ada746736b71073dd9
Kahn <255939722+kahnimoto@users.noreply.github.com>
2026-02-02 10:26:19 +0100

    missing bullets

--- G3_River_Raid/00_globals/global_game_manager.gd
+++ G3_River_Raid/00_globals/global_game_manager.gd
@@ -16,19 +16,34 @@ var movement_speed: float = 300.0:
 		movement_speed = v
 		Events.speed_changed.emit(v)
 var started := false
+var game_over := false
+
+
+func _ready() -> void:
+	Events.lives_changed.connect(_on_player_life_changed)
+
+
+func _on_player_life_changed(new_value: int) -> void:
+	if not game_over and new_value <= 0:
+		game_over = true
+		Events.game_over.emit()
+
+func restart() -> void:
+	started = false
+	game_over = false
+	get_tree().reload_current_scene()
+	Events.level_loaded.emit()
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
-		get_tree().quit()
-	if event.is_action_pressed("ui_accept"):
-		started = false
-		get_tree().reload_current_scene()
-		Events.level_loaded.emit()
-	if event is InputEventMouseButton and not started:
+	if not started and event is InputEventMouseButton:
 		started = true
 		Events.player_started.emit()
-	if event is InputEventKey:
+	elif event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+	elif event.is_action_pressed("ui_accept"):
+		restart()
+	elif event is InputEventKey:
 		if event.is_action_pressed("hotkey_1"):
 			movement_type = MovementTypes.DIRECT
 		elif event.is_action_pressed("hotkey_2"):
@@ -37,7 +52,7 @@ func _unhandled_input(event: InputEvent) -> void:
 			movement_type = MovementTypes.CONSTANT
 		elif event.is_action_pressed("hotkey_4"):
 			movement_type = MovementTypes.INDEPENDANT
-	if event is InputEventMouse:
+	elif event is InputEventMouse:
 		if (event as InputEventMouse).is_action("speed_up"):
 			movement_speed = clampf(movement_speed + 25., 50., 1000.)
 		elif (event as InputEventMouse).is_action("speed_down"):

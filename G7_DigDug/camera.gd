24656caceb00d2ded0d53b7ecd9fc48a91a29b96
Kahn <255939722+kahnimoto@users.noreply.github.com>
2026-02-13 16:45:33 +0100

    move camera with player

--- G7_DigDug/camera.gd
+++ G7_DigDug/camera.gd
@@ -4,10 +4,13 @@ const SPEED := 200.0
 
 var start_pos: Vector2
 
+@onready var player: Player = %Player
+
 func _ready() -> void:
 	start_pos = global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var direction: float = Input.get_axis("ui_up", "ui_down")
-	global_position.y = max(global_position.y + SPEED * direction * delta, start_pos.y)
+func _process(_delta: float) -> void:
+	#var direction: float = Input.get_axis("ui_up", "ui_down")
+	#global_position.y = max(global_position.y + SPEED * direction * delta, start_pos.y)
+	global_position.y = max(player.global_position.y, start_pos.y)
